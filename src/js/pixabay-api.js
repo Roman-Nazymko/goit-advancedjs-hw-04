347f0e73a8ae048fa25197d6a6a612fe5fd5877f
Roman-Nazymko <168586482+Roman-Nazymko@users.noreply.github.com>
2025-01-09 15:09:20 +0100

    homework is completed

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,19 +1,21 @@
-const PIXABAY_API_KEY = '48052326-a21cff1b214fe82d8f7d2f49f';
-const BASE_URL = 'https://pixabay.com/api/';
+import axios from 'axios';
 
-export const fetchImages = searchedQuery => {
-  const urlParams = new URLSearchParams({
-    key: PIXABAY_API_KEY,
-    q: searchedQuery,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: 200,
-  });
-  return fetch(`${BASE_URL}?${urlParams.toString()}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+const PIXABAY_API_KEY = '40905423-24d24966a8b04fca12252a818';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+export const fetchImages = (searchedQuery, currentPage, perPage) => {
+  const axiosOptions = {
+    params: {
+      key: PIXABAY_API_KEY,
+      q: searchedQuery,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: currentPage,
+      per_page: perPage,
+    },
+  };
+
+  return axios.get('', axiosOptions);
 };
